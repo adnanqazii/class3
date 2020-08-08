0f0b80fb6f0fe7c072b27d278ff68d3d844a95ff
adnanqazii <adnanqazii@users.noreply.github.com>
2020-08-08 17:33:03 +0500

    react component

---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dinner(props)
+{
+    return(
+        <div>
+            <h1>Serving {props.dishName}</h1>
+    <h1>Serving {props.sweetName}</h1>
+        </div>
+    );
+}
+export default Dinner
